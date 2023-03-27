e1bc06e96fe60fd33eff7e1de309b93c1904bdc3
Boe <boe3am@gmail.com>
2023-03-27 23:28:38 +0200

    reformatted

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,15 @@
-import "./button.m.css"
-import PropTypes from "prop-types"
+import "./button.m.css";
+import PropTypes from "prop-types";
 
-export default function Button({name, onClick}) { 
-    return (
-        <button className="btn" onClick={onClick}>{name}</button>
-    )
+export default function Button({ name, onClick }) {
+  return (
+    <button className="btn" onClick={onClick}>
+      {name}
+    </button>
+  );
 }
 
 Button.propTypes = {
-    onClick: PropTypes.func,
-    name: PropTypes.string
-}
+  onClick: PropTypes.func,
+  name: PropTypes.string,
+};
