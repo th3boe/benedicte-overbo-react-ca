376f0819d306b9496ff2d1050808013217ec1093
Boe <boe3am@gmail.com>
2023-03-27 02:44:52 +0200

    created a search, changed nav and added button function

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,31 +1,61 @@
-import { Link } from 'react-router-dom'
-import Logo from '../../../images/logo.png'
-import Search from '../../Search';
-import '../Header/header.m.css'
+import { useState } from "react";
+import { useCart } from "../../../hooks/useCart";
+import { Link } from "react-router-dom";
+import Logo from "../../../images/logo.png";
+import { BsCart2 } from "react-icons/bs";
+import "../Header/header.m.css";
 
 function Nav() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
-  export default function Header() {
-    return (
-      <header className="fixed-top">
-        <Search />
-        <div className="nav-logo">
+function NavCart() {
+  const { cart, products } = useCart();
+  return (
+    <nav>
+      <ul>
+        <li className="cart-li">
+          <Link to="/cart" className="cart-nav">
+            <BsCart2 />
+            <span className="cart-number">
+              {cart.reduce((totalQty, cart) => totalQty + cart.totalItems, 0)}
+            </span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default function Header() {
+  // const [cartCounter, setCartCounter] = useState(0);
+
+  // const cartCount = () => {
+  //   const productsIncart = JSON.parse(localStorage.getItem("CART"));
+  //   console.log("item: ", productsIncart);
+  //   // setCartCounter(cartCounter + 1);
+  //   return productsIncart.count;
+  // };
+
+  return (
+    <header className="fixed-top">
+      <div className="nav-logo">
         <img className="logo" src={Logo} alt="Logo" />
         <Nav />
-        </div>
-      </header>
-    );
-  }
+        <NavCart />
+        {/* <p>Produkter i handlekurv: </p> */}
+      </div>
+    </header>
+  );
+}
